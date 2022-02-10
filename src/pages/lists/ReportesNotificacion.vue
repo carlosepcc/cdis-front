003d5408a6149d33421792eb812f2a52c04c87b0
Carlos Enrique <carlosepcardenas@gmail.com>
2022-02-10 13:26:49 -0500

    Listar reportes de notificación

--- src/pages/lists/ReportesNotificacion.vue
+++ src/pages/lists/ReportesNotificacion.vue
@@ -11,7 +11,19 @@
 <script setup>
 import { ref } from "vue";
 import ListPage from 'components/ListPage'
+import listar from 'src/composables/useAPI'
 
 const rnotificacionArr = ref([])
-const rnotificacionFields = ref([])
+const rnotificacionFields = ref([
+  { name: 'name', required: true, label: 'Nombre', align: 'left', field: 'nombre', sortable: true },
+  { name: 'fase', required: true, label: 'Fase', align: 'left', field: 'fase', sortable: true },
+  { name: 'disciplina', required: true, label: 'Disciplina', align: 'left', field: 'disciplina', sortable: true },
+  { name: 'local', required: true, label: 'Local', align: 'left', field: 'local', sortable: true },
+  { name: 'date', required: true, label: 'Fecha', align: 'left', field: 'fecha', sortable: true },
+  { name: 'description', required: true, label: 'Descripción', align: 'left', field: 'description', sortable: true },
+])
+
+
+//fill rtecnicosArr
+listar(rnotificacionArr, '/reportenotificacion')
 </script>
