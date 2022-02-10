397809107c4707a9c9406a7454bf0cc27aa67150
Carlos Enrique <carlosepcardenas@gmail.com>
2022-02-10 13:12:03 -0500

    Listar reportes técnicos

---
+++ docs/entidades.md
@@ -0,0 +1,58 @@
+## Entidades
+
+### Usuario
+- username*: String
+- rol: Enum ("Administrador" | "Coordinador de calidad" | "Asesor de calidad" | "Encargado de proyecto" | "Revisor")
+
+## Artefacto
+- id*
+- name: String
+- descripcion: String
+- fase: Int (1 | 2 | 3)
+- disciplina: Int (1 | 2 | 3)
+- adjuntos: Array: String
+
+## Dictámen técnico
+- id*
+- proyecto: String
+- encargado: Usuario
+- descripcion: String
+- noconformidades: Int
+- oportunidades: Int
+- estado: Int (1 | 2 | 3)
+
+## Hallazgo
+- id*
+- producto: String
+- ubicacion: String
+- descripcion: String
+- impacto: Int (1 | 2 | 3)
+- date: Date
+
+## Minuta de reunión
+- id*
+- proyecto: String
+- revisor: Usuario
+- encargado: Usuario
+- descripcion: String
+
+## Reporte de notificación
+- id*
+- name: String
+- fase: Int (1 | 2 | 3)
+- disciplina: Int (1 | 2 | 3)
+- local: String
+- date: Date
+- descripcion: String
+
+## Reporte técnico
+- id*
+- name: String
+- estado: Int (1 | 2 | 3)
+- tipo: Int (1 | 2 | 3)
+- local: String
+- inicio: Date
+- cumplimiento: Date
+- evaluacion: Int (1 | 2 | 3)
+- descripcion: String
+- adjuntos: Array: String
--- src/pages/lists/ReportesTecnicos.vue
+++ src/pages/lists/ReportesTecnicos.vue
@@ -7,6 +7,22 @@
 <script setup>
 import { ref } from "vue";
 import ListPage from 'components/ListPage'
+import listar from 'src/composables/useAPI'
+
 const rtecnicosArr = ref([])
-const rtecnicoFields = ref([])
+const rtecnicoFields = ref([
+  { name: 'name', required: true, label: 'Nombre', align: 'left', field: 'nombre', sortable: true },
+  { name: 'estado', required: true, label: 'Estado', align: 'left', field: 'estado', sortable: true },
+  { name: 'tipo', required: true, label: 'tipo', align: 'left', field: 'tipo', sortable: true },
+  { name: 'local', required: true, label: 'local', align: 'left', field: 'local', sortable: true },
+  { name: 'inicio', required: true, label: 'inicio', align: 'left', field: 'inicio', sortable: true },
+  { name: 'cumplimiento', required: true, label: 'cumplimiento', align: 'left', field: 'cumplimiento', sortable: true },
+  { name: 'evaluacion', required: true, label: 'evaluacion', align: 'left', field: 'evaluacion', sortable: true },
+  { name: 'description', required: true, label: 'Descripcion', align: 'left', field: 'descripcion', sortable: true },
+  { name: 'adjunto', required: true, label: 'adjunto', align: 'left', field: 'adjunto', sortable: true },
+
+])
+
+//fill rtecnicosArr
+listar(rtecnicosArr, '/reportetecnico')
 </script>
